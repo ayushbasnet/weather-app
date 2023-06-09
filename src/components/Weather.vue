572e214f0df9f264afe33e4052ab181d9b4d9ecb
Ayush-Basnet <ayushbasnet@github.com>
2023-06-09 15:54:02 +0545

    Added Button For Different Temperature Scales

--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -18,11 +18,33 @@
           {{ place }}: {{ weather.latitude }}
           {{ weather.longitude }}
         </div>
-        <div class="date">{{ weather.timezone }}</div>
+        <div class="date">{{ weather.timezone }}{{ code.code }}</div>
       </div>
       <div class="weather-box">
         <div class="temp">
-          {{ Math.round(weather.current_weather.temperature) }}°
+          {{ Math.round(temperature) }}°
+
+          <button
+            id="celsius"
+            :class="cel ? 'selected' : 'deselected'"
+            @click="celsiusShow()"
+          >
+            C
+          </button>
+          <button
+            id="fehren"
+            :class="feh ? 'selected' : 'deselected'"
+            @click="celsiusToFahrenheit()"
+          >
+            F
+          </button>
+          <button
+            id="fehren"
+            :class="kel ? 'selected' : 'deselected'"
+            @click="celsiusToKelvin()"
+          >
+            K
+          </button>
         </div>
         <div class="weather">
           {{ code.description }}
@@ -38,9 +60,13 @@ export default {
   data() {
     return {
       imageUrl:
-        "https://images.pexels.com/photos/209831/pexels-photo-209831.jpeg?cs=srgb&dl=pexels-pixabay-209831.jpg&fm=jpg",
+        "https://images.unsplash.com/photo-1464618663641-bbdd760ae84a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8d2Vic2l0ZSUyMGJhY2tncm91bmR8ZW58MHx8MHx8fDA%3D&w=1000&q=80",
       url: "https://api.open-meteo.com/v1/forecast?",
       place: "",
+      cel: true,
+      feh: false,
+      kel: false,
+      temperature: 0,
       lat: "28.00",
       lon: "84.00",
       selected: "Nepal",
@@ -51,6 +77,8 @@ export default {
         { id: 4, text: "India", latitude: "22.00", longitude: "79.00" },
         { id: 5, text: "California", latitude: "38.30", longitude: "-76.51" },
         { id: 6, text: "Mexico", latitude: "23.00", longitude: "-102.00" },
+        { id: 7, text: "Australia", latitude: "-25.00", longitude: "135.00" },
+        { id: 8, text: "Thailand", latitude: "15.50", longitude: "101.00" },
       ],
       weathercode: [
         { code: 0, description: "Clear Sky" },
@@ -99,23 +127,79 @@ export default {
         (weatherCodes) =>
           weatherCodes.code === this.weather.current_weather.weathercode
       );
+      this.temperature = Math.round(this.weather.current_weather.temperature);
       this.changeImage();
     },
     changeImage() {
       switch (this.code.code) {
         case 0:
           this.imageUrl =
-            "https://www.survivingwithandroid.com/wp-content/uploads/2014/11/android_weather_app.jpg";
+            "https://static.vecteezy.com/system/resources/previews/003/692/649/large_2x/beautiful-clear-blue-sky-in-summer-look-lke-heaven-free-photo.jpg";
           break;
-        case (1, 2, 3):
+        case 1:
+        case 2:
+        case 3:
           this.imageUrl =
-            "https://www.survivingwithandroid.com/wp-content/uploads/2014/11/android_weather_app.jpg";
+            "https://images.pexels.com/photos/96622/pexels-photo-96622.jpeg?cs=srgb&dl=pexels-photomix-company-96622.jpg&fm=jpg";
+          break;
+        case 45:
+        case 48:
+          this.imageUrl =
+            "https://images.unsplash.com/photo-1486184885347-1464b5f10296?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1168&q=80";
+          break;
+        case 51:
+        case 53:
+        case 55:
+          this.imageUrl =
+            "https://gray-kcrg-prod.cdn.arcpublishing.com/resizer/huKOOqk_7JZ1NDAJBXbuMgyuDU4=/1200x675/smart/filters:quality(85)/cloudfront-us-east-1.images.arcpublishing.com/gray/2IK4VGMVRFJEPMLI3PODX37CSA.jpg";
+          break;
+        case 56:
+        case 57:
+          this.imageUrl =
+            "https://www.langleyadvancetimes.com/wp-content/uploads/2020/02/20505369_web1_langley-weather-cloudy-cloud.jpg";
+          break;
+        case 61:
+        case 63:
+        case 65:
+          this.imageUrl =
+            "https://img.freepik.com/premium-photo/overcast-sky-dramatic-gray-sky-white-clouds-before-rain-rainy-season-cloudy-moody-sky_33867-2091.jpg?w=2000";
+          break;
+        case 66:
+        case 67:
+          this.imageUrl =
+            "https://www.rcinet.ca/eye-on-the-arctic/wp-content/uploads/sites/30/2018/10/freezing-rain-clouds-finland.jpg";
+          break;
+        case 80:
+        case 81:
+        case 82:
+          this.imageUrl =
+            "https://cdn.pixabay.com/photo/2015/07/27/20/22/rain-863448_1280.jpg";
           break;
         default:
           this.imageUrl =
             "https://images.unsplash.com/photo-1496450681664-3df85efbd29f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fHdlYXRoZXJ8ZW58MHx8MHx8fDA%3D&w=1000&q=80";
           break;
       }
+    },
+    celsiusShow() {
+      this.temperature = Math.round(this.weather.current_weather.temperature);
+      this.cel = true;
+      this.feh = false;
+      this.kel = false;
+    },
+    celsiusToFahrenheit() {
+      this.temperature =
+        (Math.round(this.weather.current_weather.temperature) * 9) / 5 + 32;
+      this.cel = false;
+      this.feh = true;
+      this.kel = false;
+    },
+    celsiusToKelvin() {
+      this.temperature =
+        Math.round(this.weather.current_weather.temperature) + 273.15;
+      this.cel = false;
+      this.feh = false;
+      this.kel = true;
     },
   },
 };
